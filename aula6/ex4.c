2ceb6ef0dd549097c4e3b1eaf4526063d4a22316
Aluno <gabrielzontabatista@gmail.com>
2022-08-31 10:40:05 -0300

    Senai FPOO

--- aula6/ex4.c
+++ aula6/ex4.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <locale.h>
+#include <time.h>
 
 int main(){
 	setlocale(LC_ALL,"");
+	srand(time(NULL));
 	int vetor[10];
-	int i, x;
+	int i, num, encontrado = 0;
 	
-	for(i = 0; i < 10; i++){
-		printf("Digite os números para o vetor: ");
-		scanf("%d", &vetor[i]);
-	}
-	system("cls");
-	
-	printf("números do vetor: ");
-	for(i = 0; i < 10; i++){
-		printf("%d ", vetor[i]);
+	for (i = 0; i < 10; i++){
+		vetor[i] = rand();
+		printf("[%d]%d\n", i, vetor[i]);
 	}
 	
-	printf("\nDigite o número que deseja localizar: ");
-	scanf("%d", &x);
-	
-	printf("O número do vetor é:%d", vetor[x]);
+	printf("\n\nDigite um número inteiro: \n");
+	scanf("%d", &num);
+	i = 0;
+	while(!encontrado){
+		if (vetor[i] == num)
+			encontrado = 1;
+			i++;
+			if(i == 10) break;
+	}
+	if(encontrado) printf("Encontrei");
+	else printf("Não encontrei");
+
 }
