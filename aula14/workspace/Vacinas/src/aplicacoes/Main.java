73c602dcce81f50c4f87682d13185705d4b935f8
Aluno <gabrielzontabatista@gmail.com>
2022-11-09 11:29:42 -0300

    Senai FPOO

--- aula14/workspace/Vacinas/src/aplicacoes/Main.java
+++ aula14/workspace/Vacinas/src/aplicacoes/Main.java
@@ -1,102 +1,115 @@
 package aplicacoes;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
 	static Scanner scan = new Scanner(System.in);
 	static ArrayList<Vacina> vacinas = new ArrayList<>();
 	static int menu = 0;
+	static String opcoes[] = { "1. Casastrar Vacina", "2. Listar Todas", "3. Alterar Dados", "4. Excluir Registro",
+			"5. Buscar por Pet", "6. Buscar por Veterinário", "7. Buscar por Vacina", "8. Terminar Programa" };
 
-	public static void main(String[] args) {
-		
-		
-			while (menu != 8) {
-				System.out.println("\n1.Cadastrar vacina");
-				System.out.println("2.Listar vacinas");
-				System.out.println("3.Alterar dados");
-				System.out.println("4.Excluir registro");
-				System.out.println("5.Buscar por pet");
-				System.out.println("6.Buscar por veterinario");
-				System.out.println("7.Buscar por vacina");
-				System.out.println("8.Terminar programa");
-				menu = scan.nextInt();
-				switch (menu) {
-				case 1:
-					System.out.println("Nome do pet\tQuem aplicou a vacina\tNome da vacina\tData da aplicação");
-					create();
-					break;
-				case 2:
-					System.out.println("Nome do pet\tQuem aplicou a vacina\tNome da vacina\tData da aplicação");
-					read();
-					break;
-				case 3:
-					System.out.println("Digite qual pet deseja alterar:");
-					update(scan.nextInt());
-					break;
-				case 4:
-					System.out.println("Digite qual registro de pet deseja excluir:");
-					delete(scan.nextInt());
-					break;
-				case 5:
-					System.out.println("Digite qual resgistro de pet deseja buscar: ");
-					 for(int i = 0; i < vacinas.size(); i++){
-	                       Vacina vacinas = vacinas.getNomedoPet();
-	                      if(vacinas.contains(vacinas)){
-	                          System.out.println("Contém na lista: "+vacinas);
-	                      }
-					 }
-					read();
-					break;
-				case 6:
-					System.out.println("Digite qual veterinario deseja buscar:");
-					read();
-					break;
-				case 7:
-					System.out.println("Digite por qual vacina deseja buscar:");
-					read();
-					break;
-				case 8:
-					System.out.println("Obrigado por ultilizar nosso registro. Tchau.");
-				default:
-					break;
+	public static void itens(String[] itens) {
+		for (String i : itens)
+			System.out.println(i);
+	}
+
+	public static void cadastrar() throws ParseException {
+		System.out.println("Quantas vacinas deseja cadastrar:");
+		int quantas = scan.nextInt();
+		System.out.println("Nome do Pet\tVeterinário\tVacina\tData");
+		for (int i = 0; i < quantas; i++)
+			vacinas.add(new Vacina(scan.next(), scan.next(), scan.next(), scan.next()));
+	}
+
+	public static void listarTodas() {
+		System.out.println("Indice\tPet\tVeterinário\tVacina\tData\tDias");
+		for (int i = 0; i < vacinas.size(); i++) {
+			System.out.println(i + "\t" + vacinas.get(i));
+		}
+	}
+	
+	public static void alterar() throws ParseException {
+		System.out.print("Digite o índice da vacina: ");
+		int indice = scan.nextInt();
+		System.out.println("Indice\tPet\tVeterinário\tVacina\tData\tDias");
+		System.out.println(indice + "\t" + vacinas.get(indice));
+		vacinas.set(indice, new Vacina(scan.next(), scan.next(), scan.next(), scan.next()));
+		System.out.println("Registro alterado");
+	}
+
+	public static void excluir() {
+		System.out.print("Digite o índice da vacina: ");
+		int indice = scan.nextInt();
+		vacinas.remove(indice);
+		System.out.println("Registro excluído");
+	}
+
+	public static void buscarPet() {
+		System.out.print("Digite o nome do Pet: ");
+		String pet = scan.next();
+		System.out.println("Indice\tPet\tVeterinário\tVacina\tData\tDias");
+		for (int i = 0; i < vacinas.size(); i++) {
+			if(vacinas.get(i).getNomedoPet().contains(pet))
+				System.out.println(i + "\t" + vacinas.get(i));
+		}
+	}
+	
+	public static void buscarVet() {
+		System.out.print("Digite o nome do Veterinário: ");
+		String vet = scan.next();
+		System.out.println("Indice\tPet\tVeterinário\tVacina\tData\tDias");
+		for (int i = 0; i < vacinas.size(); i++) {
+			if(vacinas.get(i).getVeterinario().contains(vet))
+				System.out.println(i + "\t" + vacinas.get(i));
+		}
+	}
+	
+	public static void buscarVacina() {
+		System.out.print("Digite o nome da Vacina: ");
+		String vacina = scan.next();
+		System.out.println("Indice\tPet\tVeterinário\tVacina\tData\tDias");
+		for (int i = 0; i < vacinas.size(); i++) {
+			if(vacinas.get(i).getNomeVacina().contains(vacina))
+				System.out.println(i + "\t" + vacinas.get(i));
+		}
+	}
+	
+	public static void main(String[] args) throws ParseException {
+		while (menu != 8) {
+			itens(opcoes);
+			menu = scan.nextInt();
+			switch (menu) {
+			case 1:
+				cadastrar();
+				break;
+			case 2:
+				listarTodas();
+				break;
+			case 3:
+				alterar();
+				break;
+			case 4:
+				excluir();
+				break;
+			case 5:
+				buscarPet();
+				break;
+			case 6:
+				buscarVet();
+				break;
+			case 7:
+				buscarVacina();
+				break;
+			case 8:
+				System.out.println("Tchaw!");
+				break;
+			default:
+				System.out.println("Opção inválida");
 			}
 		}
 	}
-	
-	public static void create() {
-		Vacina v = new Vacina();
-		v.setNomedoPet(scan.next());
-		v.setVeterinario(scan.next());
-		v.setNomeVacina(scan.next());
-		v.setData(scan.next());
-		vacinas.add(v);
-	}
-	
-	public static void read() {
-		for(Vacina v: vacinas) {
-			System.out.println(v.toString());
-		}
-	}
-	public static void update(int indice) {
-		if(indice >= 0 && indice < vacinas.size()) {
-			System.out.println(vacinas.get(indice).toString());
-			Vacina v = new Vacina();
-			v.setNomedoPet(scan.next());
-			v.setVeterinario(scan.next());
-			v.setNomeVacina(scan.next());
-			v.setData(scan.next());
-			vacinas.set(indice, v);
-			System.out.println("Venda alterada");
-		}else
-			System.out.println("Item invalido");
-	}
-	public static void delete(int indice) {
-		if(indice >= 0 && indice < vacinas.size()) {
-			vacinas.remove(indice);
-			System.out.println("Item excluído");
-		}else
-			System.out.println("Item invalido");
-	}
-
 }
--- aula14/workspace/Vacinas/src/aplicacoes/Vacina.java
+++ aula14/workspace/Vacinas/src/aplicacoes/Vacina.java
@@ -1,45 +1,64 @@
 package aplicacoes;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Vacina {
-	static String nomedoPet;
-	static String veterinario;
-	static String nomeVacina;
-	static String data;
 	
+	private String nomedoPet;
+	private String veterinario;
+	private String nomeVacina;
+	private Calendar data = Calendar.getInstance();
 	
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	public Vacina(String nomedoPet, String veterinario, String nomeVacina, String data) throws ParseException {
+		this.nomedoPet = nomedoPet;
+		this.veterinario = veterinario;
+		this.nomeVacina = nomeVacina;
+		this.data.setTime(sdf.parse(data));
+	}
+
 	public String getNomedoPet() {
 		return nomedoPet;
 	}
+
 	public void setNomedoPet(String nomedoPet) {
-		Vacina.nomedoPet = nomedoPet;
+		this.nomedoPet = nomedoPet;
 	}
+
 	public String getVeterinario() {
 		return veterinario;
 	}
+
 	public void setVeterinario(String veterinario) {
-		Vacina.veterinario = veterinario;
+		this.veterinario = veterinario;
 	}
+
 	public String getNomeVacina() {
 		return nomeVacina;
 	}
+
 	public void setNomeVacina(String nomeVacina) {
-		Vacina.nomeVacina = nomeVacina;
+		this.nomeVacina = nomeVacina;
 	}
-	public String getData() {
+
+	public Calendar getData() {
 		return data;
 	}
-	public void setData(String data) {
-		Vacina.data = data;
+
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	int vacinadoAquantosDias() {
+		Calendar hoje = Calendar.getInstance();
+		return hoje.get(Calendar.DAY_OF_YEAR) - data.get(Calendar.DAY_OF_YEAR);
 	}
 	
 	public String toString() {
-		return String.format("\t%s\t\t%s\t\t%s\t\t%s\t", nomedoPet, veterinario, nomeVacina, data);
+		return String.format("%s\t%s\t%s\t%s\t%d", nomedoPet, veterinario, nomeVacina, sdf.format(data.getTime()), vacinadoAquantosDias());
 	}
-	
-	
-	
-	
-	
 	
 }
