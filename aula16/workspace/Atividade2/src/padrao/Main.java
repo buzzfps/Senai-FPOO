5183e87fb1fec11e4c71b75db0a990947a49b96e
Aluno <gabrielzontabatista@gmail.com>
2022-11-25 08:33:45 -0300

    Senai FPOO

--- aula16/workspace/Atividade2/src/padrao/Main.java
+++ aula16/workspace/Atividade2/src/padrao/Main.java
@@ -1,9 +1,9 @@
 package padrao;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 public class Main {
 	
 	static BufferedReader br;
@@ -11,19 +11,14 @@ public class Main {
 	
 	public static void main(String[] args) {
 		try {
-			br = new BufferedReader(new FileReader("./bd/dados.txt"));
+			br = new BufferedReader(new FileReader("./bd/dados2.csv"));
 			String linha = br.readLine();
 			while(linha != null) {
-				String palavras[] = linha.split(" ");
-				System.out.print(palavras[0]+"\t"+palavras[1]);
-				System.out.print(linha);
-				i++;
-				System.out.print("\tA linha possui "+linha.length()+" letras\t");
-				System.out.println(+palavras.length+" palavras");
+				String coluna[] = linha.split(";");
+				System.out.println(coluna[0]+"\t"+coluna[1]+"\t"+coluna[2]);
 				linha = br.readLine();
+				i++;
 			}
-			
-			
 			br.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("Arquivo não encontrado, erro: "+e);
@@ -31,13 +26,6 @@ public class Main {
 			System.out.println("Erro ao ler o arquivo, erro: "+e);
 		}
 		
-		System.out.printf("A quantidade de linhas do programa é : %d", i);
+		System.out.printf("A quantidade de linhas do programa é : %d", i);	  
 	}
 }
-
-
-
-
-
-
-
